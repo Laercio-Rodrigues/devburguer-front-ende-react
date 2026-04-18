2817d2df04c3567ffb529ddaeec310be109bc0c3
Laércio-Rodrigues <laerrcioo91@gmail.com>
2026-04-18 05:43:14 -0400

    Estilos Globais com Styled-Components

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Login } from './containers/Login';
 
+import GlbalStyles from './styles/globalStyles'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Login />
+    <GlbalStyles />
   </StrictMode>,
 );
