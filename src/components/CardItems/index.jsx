4d784f0670c53438802a0d8a618ced54163006de
Laércio-Rodrigues <laerrcioo91@gmail.com>
2026-06-29 12:16:10 -0400

    Finalizando a parte visual do pedido

--- src/components/CardItems/index.jsx
+++ src/components/CardItems/index.jsx
@@ -1,8 +1,16 @@
+import TrashIcon from '../../assets/trash.svg';
 import { useCart } from '../../hooks/CartContext';
+import { formatPrice } from '../../utils/formatPrice';
 import { Table } from '../index';
-
+import {
+	ButtonGroup,
+	EmptyCart,
+	ProductImage,
+	ProductTotalPrice,
+	TrashImage,
+} from './styles';
 export function CartItems() {
-	const { cartProducts, decreaseProducts, increaseProduct } = useCart();
+	const { cartProducts, decreaseProducts, increaseProduct, deleteProducts } = useCart();
 
 	console.log(cartProducts);
 	return (
@@ -14,6 +22,7 @@ export function CartItems() {
 					<Table.Th>Preço</Table.Th>
 					<Table.Th>Quantidade</Table.Th>
 					<Table.Th>Total</Table.Th>
+					<Table.Th></Table.Th>
 				</Table.Tr>
 			</Table.Header>
 			<Table.Body>
@@ -21,15 +30,45 @@ export function CartItems() {
 					cartProducts.map((product) => (
 						<Table.Tr key={product.id}>
 							<Table.Td>
-								<img src={product.url} alt='imagem-items' />
+								<ProductImage src={product.url} alt="imagem-items" />
 							</Table.Td>
 							<Table.Td>{product.name}</Table.Td>
-                            <Table.Td>{product. currencyValue}</Table.Td>
-                            <Table.Td>{product. quantity}</Table.Td>
+							<Table.Td>{product.currencyValue}</Table.Td>
+							<Table.Td>
+								<ButtonGroup>
+									<button
+										type="button"
+										onClick={() => decreaseProducts(product.id)}
+									>
+										-
+									</button>
+
+									{product.quantity}
+									<button
+										type="button"
+										onClick={() => increaseProduct(product.id)}
+									>
+										+
+									</button>
+								</ButtonGroup>
+							</Table.Td>
+
+							<Table.Td>
+								<ProductTotalPrice>
+									{formatPrice(product.quantity * product.price)}
+								</ProductTotalPrice>
+							</Table.Td>
+							<Table.Td>
+								<TrashImage
+									src={TrashIcon}
+									alt="Lixeira"
+									onClick={() => deleteProducts(product.id)}
+								/>
+							</Table.Td>
 						</Table.Tr>
 					))
 				) : (
-					<div>Carrinho Vazio</div>
+					<EmptyCart>Carrinho Vazio</EmptyCart>
 				)}
 			</Table.Body>
 		</Table.Root>
