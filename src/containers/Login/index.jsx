10e81845b4997f98efec0ffa885d969e93af39e8
Laércio-Rodrigues <laerrcioo91@gmail.com>
2026-07-07 06:33:46 -0400

    Começando a Tela de Admin

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,12 +1,12 @@
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
-import * as yup from "yup";
-import Logo from "../../assets/logo.svg";
-import { Button } from "../../components/Button";
-import { useUser } from "../../hooks/UserContext";
-import { api } from "../../services/api";
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import * as yup from 'yup';
+import Logo from '../../assets/logo.svg';
+import { Button } from '../../components/Button';
+import { useUser } from '../../hooks/UserContext';
+import { api } from '../../services/api';
 import {
 	Container,
 	Form,
@@ -15,7 +15,7 @@ import {
 	Link,
 	RightContainer,
 	Title,
-} from "./styles";
+} from './styles';
 
 export function Login() {
 	const navigate = useNavigate();
@@ -25,12 +25,12 @@ export function Login() {
 		.object({
 			email: yup
 				.string()
-				.email("Digite um e-mail válido")
-				.required("O e-mail é obrigatório"),
+				.email('Digite um e-mail válido')
+				.required('O e-mail é obrigatório'),
 			password: yup
 				.string()
-				.min(6, "A senha deve ter pelo menos 6 caracteres")
-				.required("Digite uma senha"),
+				.min(6, 'A senha deve ter pelo menos 6 caracteres')
+				.required('Digite uma senha'),
 		})
 		.required();
 
@@ -46,23 +46,27 @@ export function Login() {
 
 	const onSubmit = async (data) => {
 		const { data: userData } = await toast.promise(
-			api.post("/sessions", {
+			api.post('/sessions', {
 				email: data.email,
 				password: data.password,
 			}),
 			{
-				pending: "Verificando seus dados",
+				pending: 'Verificando seus dados',
 
 				success: {
 					render() {
 						setTimeout(() => {
-							navigate("/");
+							if (userData?.admin) {
+								navigate('/admin/home');
+							} else {
+								navigate('/');
+							}
 						}, 2000);
 
 						return `Seja Bem-vindo(a) 👌`;
 					},
 				},
-				error: "Email ou Senha Incorretos 🤯",
+				error: 'Email ou Senha Incorretos 🤯',
 			},
 		);
 		putUserData(userData);
@@ -83,7 +87,7 @@ export function Login() {
 					<InputContainer>
 						<label>
 							Email
-							<input type="email" {...register("email")} />
+							<input type="email" {...register('email')} />
 						</label>
 						<p>{errors?.email?.message}</p>
 					</InputContainer>
@@ -91,7 +95,7 @@ export function Login() {
 					<InputContainer>
 						<label>
 							Senha
-							<input type="password" {...register("password")} />
+							<input type="password" {...register('password')} />
 						</label>
 						<p>{errors?.password?.message}</p>
 					</InputContainer>
