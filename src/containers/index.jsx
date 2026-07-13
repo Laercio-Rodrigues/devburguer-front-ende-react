583744df0c310b773c68f17e509783a88b9eb7ea
Laércio-Rodrigues <laerrcioo91@gmail.com>
2026-07-13 06:58:32 -0400

    alteraçoes 3

---
+++ src/containers/index.jsx
@@ -0,0 +1,23 @@
+// Debug: listando todos os containers
+console.log('Containers:', Object.keys(import.meta.glob('./*/*.jsx')));
+
+export * from "./Cart/index.jsx";
+export * from "./Checkout/index.jsx";
+export * from "./Home/index.jsx";
+export * from "./Login/index.jsx";
+export * from "./Menu/index.jsx";
+export * from "./Register/index.jsx";
+
+
+
+// export * from './Admin/EditProduct';
+// export * from './Admin/NewProduct';
+// export * from './Admin/Orders';
+// export * from './Admin/Products';
+// export * from './Cart';
+// export * from './Checkout';
+// export * from './CompletePayment';
+// export * from "./Home/index.jsx";
+// export * from './Login';
+// export * from './Menu';
+// export * from './Register';
